f9b5d52c082b1fdbe61ab3645acae5b441960f02
Gunock <asz.czyk@gmail.com>
2021-03-06 14:54:17 +0100

    Improved graph drawing

--- proj1/main.py
+++ proj1/main.py
@@ -6,23 +6,29 @@ from tsplib95.models import StandardProblem
 from proj1.classes.NearestNeighbourProblemSolver import NearestNeighbourProblemSolver
 from proj1.classes.ProblemSolver import ProblemSolver
 
+_EXPERIMENT_COUNT: int = 50
 
-def _draw_graph(problem: StandardProblem, path: List[int], file_name: str) -> None:
+
+def _draw_graph(problem: StandardProblem, path: List[int], file_name: str, start_node: int = -1) -> None:
     import matplotlib.pyplot as plt
     import networkx as nx
 
     graph = nx.Graph()
 
     node_coords_dict = dict(problem.node_coords)
+    node_colors = []
     for node_index in node_coords_dict:
         graph.add_node(node_index, pos=tuple(node_coords_dict[node_index]))
+        node_colors.append('red' if node_index == start_node else 'blue')
 
     for i in range(len(path) - 1):
         graph.add_edge(path[i], path[i + 1])
 
     pos = nx.get_node_attributes(graph, 'pos')
 
-    nx.draw(graph, pos, node_size=1, edge_color='r', with_labels=True, font_size=5)
+    nx.draw_networkx_edges(graph, pos, edge_color='red')
+    nx.draw_networkx_nodes(graph, pos, node_size=1, node_color=node_colors)
+    nx.draw_networkx_labels(graph, pos, font_size=5)
     plt.savefig(file_name)
     plt.clf()
 
@@ -49,7 +55,7 @@ def solve_problem(problem: StandardProblem,
     if not os.path.exists('./graphs/'):
         os.makedirs('./graphs/')
 
-    _draw_graph(problem, [index + 1 for index in path], f'./graphs/{file_name}.pdf')
+    _draw_graph(problem, [index + 1 for index in path], f'./graphs/{file_name}.pdf', start_node)
 
 
 def run_experiment(problem: StandardProblem, problem_solver: ProblemSolver, file_name: str = "graph"):
@@ -60,13 +66,14 @@ def run_experiment(problem: StandardProblem, problem_solver: ProblemSolver, file
     :param file_name: name of file to which result graph will be saved
     """
     import random
+    global _EXPERIMENT_COUNT
 
     random_nodes: Set[int] = set()
 
-    if problem.dimension < 50:
+    if problem.dimension < _EXPERIMENT_COUNT:
         raise ValueError("problem.dimension < 50")
 
-    while len(random_nodes) < 50:
+    while len(random_nodes) < _EXPERIMENT_COUNT:
         random_node = random.randint(0, problem.dimension)
 
         if random_node not in random_nodes:
