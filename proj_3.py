1ed14bc1efeec0db9c9e626c387d8cc31b39f61b
Gunock <asz.czyk@gmail.com>
2021-04-17 23:20:55 +0200

    Added some useless ScoreSteepSearch changes

--- proj_3.py
+++ proj_3.py
@@ -4,9 +4,9 @@ import tsplib95
 from tsplib95.models import StandardProblem
 
 from common.experiments import run_experiment_local_search
-from solvers.local_search_improved import CandidateSteepSearch, ScoreSteepSearch
+from solvers.local_search_improved import ScoreSteepSearch
 
-_EXPERIMENT_COUNT: int = 100
+_EXPERIMENT_COUNT: int = 1
 
 
 def main():
@@ -15,10 +15,13 @@ def main():
 
     shutil.rmtree("./graphs/", ignore_errors=True)
 
-    run_experiment_local_search(problem_a, CandidateSteepSearch(), "kroa200_css", _EXPERIMENT_COUNT)
-    run_experiment_local_search(problem_b, CandidateSteepSearch(), "krob200_css", _EXPERIMENT_COUNT)
-    run_experiment_local_search(problem_a, ScoreSteepSearch(), "kroa200_sss", _EXPERIMENT_COUNT)
+    # run_experiment_local_search(problem_a, CandidateSteepSearch(), "kroa200_css", _EXPERIMENT_COUNT)
+    # run_experiment_local_search(problem_b, CandidateSteepSearch(), "krob200_css", _EXPERIMENT_COUNT)
+    # run_experiment_local_search(problem_a, ScoreSteepSearch(), "kroa200_sss", _EXPERIMENT_COUNT)
     run_experiment_local_search(problem_b, ScoreSteepSearch(), "krob200_sss", _EXPERIMENT_COUNT)
+    # run_experiment_local_search(problem_b, EdgeSwapSteepSearch(), "krob200_sss", _EXPERIMENT_COUNT)
+    # run_experiment_constructive(problem_a, GreedyCycleProblemSolver(), "kroa200_gc", _EXPERIMENT_COUNT)
+    # run_experiment_constructive(problem_b, GreedyCycleProblemSolver(), "krob200_gc", _EXPERIMENT_COUNT)
 
 
 if __name__ == '__main__':
--- solvers/local_search_improved.py
+++ solvers/local_search_improved.py
@@ -60,13 +60,13 @@ def _find_outside_swap_move(distance_matrix: np.ndarray, cycle: list, unused_nod
     return move, cost_delta
 
 
-def _find_edge_swap_move(distance_matrix: np.ndarray, cycle: list, index_1, index_2):
-    node_1_1 = cycle[index_1]
-    node_1_2 = cycle[index_1 + 1]
+def _find_edge_swap_move(distance_matrix: np.ndarray, cycle: list, i_1, i_2):
+    node_1_1 = cycle[i_1]
+    node_1_2 = cycle[i_1 + 1]
     edge_length_1 = distance_matrix[node_1_1, node_1_2]
 
-    node_2_1 = cycle[index_2]
-    node_2_2 = cycle[index_2 + 1]
+    node_2_1 = cycle[i_2]
+    node_2_2 = cycle[i_2 + 1]
     edge_length_2 = distance_matrix[node_2_1, node_2_2]
 
     new_edge_length_1 = distance_matrix[node_1_1, node_2_1]
@@ -75,7 +75,7 @@ def _find_edge_swap_move(distance_matrix: np.ndarray, cycle: list, index_1, inde
     current_cost = edge_length_1 + edge_length_2
     new_cost = new_edge_length_1 + new_edge_length_2
     cost_delta = new_cost - current_cost
-    move = (index_1 + 1, index_2)
+    move = (i_1 + 1, i_2)
     return move, cost_delta
 
 
@@ -151,28 +151,31 @@ class CandidateSteepSearch(SearchProblemSolver):
 
 
 class ScoreSteepSearch(SearchProblemSolver):
+    def __init__(self):
+        self.cache = set()
+
     def solve(self, distance_matrix: np.ndarray) -> List[int]:
         cycle = RandomSearch(0.0).solve(distance_matrix)
         unused_nodes_list = [node for node in range(distance_matrix.shape[0]) if node not in cycle]
 
         while True:
-            move, operation, cost_delta = ScoreSteepSearch.__find_best_move(distance_matrix, cycle, unused_nodes_list)
+            move, operation, cost_delta = self.__find_best_move(distance_matrix, cycle, unused_nodes_list)
 
             if cost_delta >= 0:
                 break
 
             i_1, i_2 = move
             if operation == LocalSearchOperation.swap_outside:
+                self.cache.add((unused_nodes_list[i_2], cycle[i_1], operation))
                 cycle[i_1], unused_nodes_list[i_2] = unused_nodes_list[i_2], cycle[i_1]
                 if i_1 == 0:
                     cycle[-1] = cycle[0]
             elif operation == LocalSearchOperation.swap_edges:
+                self.cache.add((cycle[i_1], cycle[i_2], operation))
                 cycle[i_1:i_2 + 1] = reversed(cycle[i_1:i_2 + 1])
-
         return cycle
 
-    @staticmethod
-    def __find_best_move(distance_matrix: np.ndarray, cycle: list, unused_nodes_list: list):
+    def __find_best_move(self, distance_matrix: np.ndarray, cycle: list, unused_nodes_list: list):
         best_cost_delta = np.iinfo(distance_matrix.dtype).max
         best_move: tuple = tuple()
         best_operation = None
@@ -191,7 +194,7 @@ class ScoreSteepSearch(SearchProblemSolver):
             if i_1 >= len(cycle) - 2:
                 continue
 
-            move, cost_delta = ScoreSteepSearch.__find_best_edge_swap_move(distance_matrix, cycle, i_1)
+            move, cost_delta = self.__find_best_edge_swap_move(distance_matrix, cycle, i_1)
             if cost_delta < best_cost_delta:
                 best_cost_delta = cost_delta
                 best_move = move
@@ -199,13 +202,66 @@ class ScoreSteepSearch(SearchProblemSolver):
 
         return best_move, best_operation, best_cost_delta
 
-    @staticmethod
-    def __find_best_edge_swap_move(distance_matrix: np.ndarray, cycle: list, i_1):
+    def __find_best_edge_swap_move(self, distance_matrix: np.ndarray, cycle: list, i_1):
         best_cost_delta = np.iinfo(distance_matrix.dtype).max
         best_move: tuple = tuple()
         for i_2 in range(i_1 + 2, len(cycle) - 1):
-            move, cost_delta = _find_edge_swap_move(distance_matrix, cycle, i_1, i_2)
+            if (cycle[i_1 + 1], cycle[i_2 + 1], LocalSearchOperation.swap_edges) in self.cache:
+                continue
+
+            move = (i_1 + 1, i_2)
+            cost_delta = self.__find_edge_swap_move(distance_matrix, cycle, i_1, i_2)
             if cost_delta < best_cost_delta:
                 best_cost_delta = cost_delta
                 best_move = move
         return best_move, best_cost_delta
+
+    @staticmethod
+    def __find_edge_swap_move(distance_matrix: np.ndarray, cycle: list, i_1, i_2):
+        node_1_1 = cycle[i_1]
+        node_1_2 = cycle[i_1 + 1]
+        edge_length_1 = distance_matrix[node_1_1, node_1_2]
+
+        node_2_1 = cycle[i_2]
+        node_2_2 = cycle[i_2 + 1]
+        edge_length_2 = distance_matrix[node_2_1, node_2_2]
+
+        new_edge_length_1 = distance_matrix[node_1_1, node_2_1]
+        new_edge_length_2 = distance_matrix[node_1_2, node_2_2]
+
+        current_cost = edge_length_1 + edge_length_2
+        new_cost = new_edge_length_1 + new_edge_length_2
+        cost_delta = new_cost - current_cost
+        return cost_delta
+
+    def __find_best_outside_swap(self, distance_matrix: np.ndarray, cycle: list, unused_nodes_list: list, i_1):
+        best_move: tuple = tuple()
+        best_cost_delta = np.iinfo(distance_matrix.dtype).max
+
+        for i_2 in range(len(unused_nodes_list)):
+            if (cycle[i_1], unused_nodes_list[i_2], LocalSearchOperation.swap_outside) in self.cache:
+                continue
+
+            move = (i_1, i_2)
+            cost_delta = self.__find_outside_swap_move(distance_matrix, cycle, unused_nodes_list, i_1, i_2)
+            if cost_delta < best_cost_delta:
+                best_cost_delta = cost_delta
+                best_move = move
+        return best_move, best_cost_delta
+
+    @staticmethod
+    def __find_outside_swap_move(distance_matrix: np.ndarray, cycle: list, unused_nodes: list, i_1, i_2):
+        node_1_0 = cycle[i_1 - 1]
+        node_1_1 = cycle[i_1]
+        node_1_2 = cycle[i_1 + 1]
+        edge_length_1_1 = distance_matrix[node_1_0, node_1_1]
+        edge_length_1_2 = distance_matrix[node_1_1, node_1_2]
+
+        node_2 = unused_nodes[i_2]
+        new_edge_length_1 = distance_matrix[node_1_0, node_2]
+        new_edge_length_2 = distance_matrix[node_2, node_1_2]
+
+        current_cost = edge_length_1_1 + edge_length_1_2
+        new_cost = new_edge_length_1 + new_edge_length_2
+        cost_delta = new_cost - current_cost
+        return cost_delta
